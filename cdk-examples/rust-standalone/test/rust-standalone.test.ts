d596d4247035a13d68f378aabca7a7db02fc9b34
Ritvik Nag <rnag@collegeboard.org>
2022-02-18 15:56:23 -0500

    Fix tests

--- cdk-examples/rust-standalone/test/rust-standalone.test.ts
+++ cdk-examples/rust-standalone/test/rust-standalone.test.ts
@@ -2,16 +2,26 @@ import * as cdk from 'aws-cdk-lib';
 import { Template, Match } from 'aws-cdk-lib/assertions';
 import * as RustStandalone from '../lib/rust-standalone-stack';
 
-test('SQS Queue and SNS Topic Created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new RustStandalone.RustStandaloneStack(app, 'MyTestStack');
-  // THEN
+test('S3 Bucket Created', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new RustStandalone.RustStandaloneStack(
+        app,
+        'MyTestStack'
+    );
+    // THEN
 
-  const template = Template.fromStack(stack);
+    const template = Template.fromStack(stack);
 
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
-  });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
+    template.hasResourceProperties('AWS::S3::Bucket', {
+        PublicAccessBlockConfiguration: {
+            BlockPublicAcls: true,
+            BlockPublicPolicy: true,
+            IgnorePublicAcls: true,
+            RestrictPublicBuckets: true,
+        },
+    });
+
+    template.resourceCountIs('AWS::S3::Bucket', 1);
+    template.resourceCountIs('AWS::Lambda::Function', 1);
 });
